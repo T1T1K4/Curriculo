32bbe35f5732a4c9c12abaccd02fa4bcb20fa2fb
Carlos Eduardo <151068498+T1T1K4@users.noreply.github.com>
2025-02-14 16:57:59 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting ){
+            entry.target.classList.add('show')
+        } else{
+            entry.target.classList.remove('show')
+        }
+    } )
+})
+
+const elementos = document.querySelectorAll('.hidden')
+
+elementos.forEach( (elementos) =>  myObserver.observe(elementos))
